83de7d80e58ca4ef590080c0e412663c3443bcbc
Valeska Marques <valeskkitty@gmail.com>
2023-08-03 09:38:45 -0300

    funcoes com parametros

--- 06-funcoes.php
+++ 06-funcoes.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Funções</title>
 </head>
+
 <body>
     <h1>Criando e usando funções</h1>
 
     <h2>Funções como procedimento ou sub-rotina</h2>
-    
 
-    <?php 
-    function dadosAutor() {
+
+    <?php
+    function dadosAutor(): void
+    {
         echo "<div>";
         echo "<p>Nome: <strong>Lucas</strong></p>";
         echo "<p><a href='mailto:teste@gmail.com'>teste@gmail.com</a></p>";
@@ -22,24 +25,71 @@
 
     <section>
         <h3>Chamada da sub-rotina/procedimento</h3>
-        <?=dadosAutor()?>
+        <?= dadosAutor() ?>
 
         <article>
             <h4>Outra chamada</h4>
-            <?=dadosAutor()?>
+            <?= dadosAutor() ?>
         </article>
     </section>
 
     <h2>Função com retorno de dados</h2>
 
-    <?php 
-    function dadosCurso() {
+    <?php
+    function dadosCurso(): string
+    {
         return "Técnico em Informática para Internet";
     }
     ?>
 
-    <p>Estamos no curso de <?=dadosCurso()?></p>
-    <p><?=dadosCurso()?> é ministrado no Senac Penha</p>
+    <p>Estamos no curso de <?= dadosCurso() ?></p>
+    <p><?= dadosCurso() ?> é ministrado no Senac Penha</p>
+
+    <h2>Função com parâmetros</h2>
+
+    <?php
+    function soma($valor1, $valor2, $valor3): float
+    {
+        return $valor1 + $valor2 + $valor3;
+    }
+    ?>
+
+    <!-- Chamando e exibindo diretamente o retorno -->
+    <p>Resultado 1: <?= soma(10, 20, 5) ?></p>
+    <p>Resultado 2: <?= soma(23, 84, 1000.75) ?></p>
+
+    <?php
+    // Chamando e guardando o retorno
+    $resultado3 = soma(500, 480, 700);
+    ?>
+
+
+    <p>Resultado 3: <?= $resultado3 ?></p>
+    <?php if ($resultado3 > 100) { ?>
+        <p>Vinicios foi demitido!</p>
+    <?php } ?>
+
+
+    <!-- Usando função como parte  -->
+    <?php 
+    if(soma(2, 3, 10) >= 10) { ?>
+       <p>Kaue foi demitido!</p>
+    <?php } ?>
+
     
+    <h2>Função com parâmetros opcionais</h2>
+    
+    <?php
+    function saudacao($mensagem, $pessoa = "") : string
+    {
+        return "Olá, $mensagem $pessoa!";
+    }
+    ?>
+
+    <p><?= saudacao("bom dia", "Melissa") ?></p>
+    <p><?= saudacao("boa noite", "Tanaka") ?></p>
+    <p><?= saudacao("boa tarde") ?></p>
+
 </body>
+
 </html>
