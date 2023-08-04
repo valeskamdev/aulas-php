d3cda28dd2528d50a5ea72a25e14c370af036fef
Valeska Marques <valeskkitty@gmail.com>
2023-08-04 11:02:51 -0300

    funcao trim()

---
+++ 07-funcoes-nativas.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções nativas</title>
+</head>
+<body>
+    <h1>Exemplos de funções nativas</h1>
+    <hr>
+
+    <h2>Strings</h2>
+    <h3>trim()</h3>
+
+    <?php 
+    // função que remove espaços em branco no início e no final de uma string
+    $nome = "    Vinicius me deve 80 paçocas    ";
+    $nomeSemEspacos = trim($nome);
+
+    var_dump($nome);
+    var_dump($nomeSemEspacos);
+    
+    ?>
+
+
+    <h2>Numéricas</h2>
+
+    <h2>Arrays</h2>
+
+    <h2>Filtros</h2>
+
+    <h2>Segurança</h2>
+
+</body>
+</html>
