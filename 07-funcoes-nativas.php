e069e54bdaf8f40f2fea6deccfbbe365e68bdf5b
Valeska Marques <valeskkitty@gmail.com>
2023-08-07 08:57:36 -0300

    filtros, validacao e sanitizacao

--- 07-funcoes-nativas.php
+++ 07-funcoes-nativas.php
@@ -12,18 +12,18 @@
     <h2>Strings</h2>
     <h3>trim()</h3>
 
-    <?php 
+    <?php
     // função que remove espaços em branco no início e no final de uma string
     $nome = "    Vinicius me deve 80 paçocas    ";
     $nomeSemEspacos = trim($nome);
 
     var_dump($nome);
     var_dump($nomeSemEspacos);
-    
+
     ?>
 
     <h3>str_replace()</h3>
-    <?php 
+    <?php
     // função que substitui uma string por outra
     $fraseFeia = "O PHP é uma linguagem ultrapassada e insegura";
     $fraseBonita = str_replace(["ultrapassada", "insegura"] , ["moderna", "segura"], $fraseFeia);
@@ -35,14 +35,14 @@
 
     <h3>explode()</h3>
 
-    <?php 
+    <?php
     // função que transforma uma string em um array
     $linguagens = "Java, PHP, JavaScript, Python, C#";
     $arrayLinguagens = explode(", ", $linguagens);
 
     var_dump($linguagens);
     var_dump($arrayLinguagens);
-    
+
     ?>
 
     <hr>
@@ -50,7 +50,7 @@
     <h2>Numéricas</h2>
     <h3>min, max, round</h3>
 
-    <?php 
+    <?php
     $valores = [10, 5, 50, 1000, 275];
     $valorQuebrado = 10.5;
 
@@ -64,15 +64,15 @@
 
     var_dump($valorQuebrado);
     var_dump($valorQuebradoArredondado);
-    
+
     ?>
 
     <hr>
 
     <h2>Arrays</h2>
     <h3>implode()</h3>
-    
-    <?php 
+
+    <?php
     // função que transforma um array em uma string
     $grupos = ["Super Junior", "Blackpink", "DAY6", "EXO", "ATEEZ"];
     $gruposString = implode(" - ", $grupos);
@@ -82,7 +82,7 @@
     ?>
 
     <h3>extract()</h3>
-    <?php 
+    <?php
     // função que transforma um array em variáveis
     $produto = [
         "nome" => "Notebook",
@@ -101,27 +101,50 @@
     </ul>
 
     <h3>array_sum()</h3>
-    <?php 
+    <?php
     // função que soma os valores de um array
     $total = array_sum($valores);
 
     var_dump($valores);
     var_dump($total);
-    
+
     ?>
 
     <h3>array_unique()</h3>
-    <?php 
+    <?php
     // função que remove valores duplicados de um array
     $produtos = ["Nootbook", "Smartphone", "Tablet", "Smartphone", "Smartwatch", "Tablet"];
     $produtosUnicos = array_unique($produtos);
 
     var_dump($produtos);
     var_dump($produtosUnicos);
-    
+
     ?>
 
     <h2>Filtros</h2>
+    <p>Recursos de análise e limpeza de dados aplicados através das funções <code>filter_var()</code> e <code>filter_input()</code>.</p>
+
+    <h3>Validação</h3>
+
+    <?php
+    $email = "leska@gmail.com.br";
+    var_dump(filter_var($email, FILTER_VALIDATE_EMAIL));
+    ?>
+
+
+    <h3>Sanitização</h3>
+
+    <?php
+    $ataque = "<script>document.body.innerHTML = '<h1>Ataque realizado com sucesso</h1>'</script>";
+
+    //    echo $ataque;
+    // execução sem sanitização (script é executado)
+
+    $ataqueSanitizado = filter_var($ataque, FILTER_SANITIZE_SPECIAL_CHARS);
+    echo $ataqueSanitizado;
+    var_dump($ataqueSanitizado);
+
+    ?>
 
     <h2>Segurança</h2>
 
