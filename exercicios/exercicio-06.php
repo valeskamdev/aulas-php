82f2c6ba064770b10e7bac20e80b43b7a73c3964
Valeska Marques <valeskkitty@gmail.com>
2023-08-09 15:45:34 -0300

    validacao php dos input's

--- exercicios/exercicio-06.php
+++ exercicios/exercicio-06.php
@@ -12,12 +12,15 @@
           background-color: #cbcbcb;
           font-size: 1.2rem;
           font-family: Arial, Helvetica, sans-serif;
+          height: 100vh;
+          width: 100%;
         }
 
-        strong {
-          text-decoration: underline;
-          text-decoration-color: red;
-          margin-right: 0.5rem;
+        h1 {
+          text-shadow: 1px 1px rgba(0, 0, 0, 0.56);
+          font-size: 2.5rem;
+          font-weight: bold;
+          color: #09941a;
         }
 
         form, article {
@@ -37,49 +40,79 @@
             4 => "Bugatti"
         ];
 
-
         if (isset($_POST['enviar'])) {
-            $nome = $_POST['nome'];
-            $fabricante = $fabricantes[$_POST['fabricante']];
-            $preco = floatval($_POST['preco']);
-            $disponibilidade = $_POST['disponibilidade'];
-            $descricao = $_POST['descricao'];
+            if(empty($_POST['nome']) || empty($_POST['preco']) || empty($_POST['fabricante'])) { ?>
+                <div class="d-flex align-items-center justify-content-center vh-100">
+                    <div class="alert alert-warning" role="alert">
+                        Precisa preencher todos os campos! <a href="javascript:history.back()">Voltar</a>
+                    </div>
+                </div>
+            <?php } else {
 
-            $precoFormatado = number_format($preco, 2, ",", ".");
+            $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+            $fabricante = filter_var($fabricantes[$_POST['fabricante']], FILTER_SANITIZE_SPECIAL_CHARS);
+            $preco = filter_input(INPUT_POST, 'preco', FILTER_SANITIZE_NUMBER_FLOAT);
+            $disponibilidade = filter_input(INPUT_POST, 'disponibilidade', FILTER_SANITIZE_SPECIAL_CHARS);
+            $descricao = filter_input(INPUT_POST, 'descricao', FILTER_SANITIZE_SPECIAL_CHARS);
+
+
             ?>
-            <div class="w-50 d-flex align-items-center justify-content-center vh-100 w-100 card-carro">
-                <article class="p-5 rounded-1">
-                    <h1 class="mb-3">Informações do carro</h1>
-                    <p><strong>Nome:</strong> <?= $nome ?></p>
-                    <p><strong>Fabricante:</strong> <?= $fabricante ?></p>
-                    <p><strong>Preço:</strong> R$<?= $precoFormatado ?></p>
-                    <p><strong>Disponibilidade:</strong><?= $disponibilidade ?></p>
-                    <p><strong>Descrição:</strong> <?= $descricao ?></p>
-                </article>
+            <div class="w-50 d-flex align-items-center justify-content-center flex-column vh-100 w-100">
+                <h1 class="mb-4">Carro cadastrado com sucesso!  ✅</h1>
+                <table class="table table-striped caption-top w-75">
+                    <caption class="text-center bg-secondary text-white">Informações do carro</caption>
+                    <tbody>
+                    <tr>
+                        <th scope="row">Nome</th>
+                        <td><?= $nome ?></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Fabricante</th>
+                        <td><?= $fabricante ?></td>
+                    </tr>
+                    <tr>
+                        <?php if(gettype($preco) != "string") {
+                        $preco = number_format($_POST['preco'], 2, ",", "."); ?>
+                        <th scope="row">Preço</th>
+                        <td>R$<?= $preco ?></td>
+                        <?php } ?>
+                    </tr>
+                    <tr>
+                        <th scope="row">Disponibilidade</th>
+                        <td><?= $disponibilidade ?></td>
+                    </tr>
+                    <tr>
+                        <?php if(!empty($_POST['descricao'])) { ?>
+                        <th scope="row">Descrição</th>
+                        <td><?= $descricao ?></td>
+                        <?php } ?>
+                    </tr>
+                    </tbody>
+                </table>
+
             </div>
-        <?php } else {  ?>
+        <?php }  }else {  ?>
 
         <div class="d-flex align-items-center justify-content-center vh-100">
             <form action="" method="post" class="w-75 rounded-1 p-5">
                 <div class="mb-3">
                     <label for="nome" class="form-label">Nome do carro</label>
-                    <input type="text" class="form-control" id="nome" name="nome">
+                    <input type="text" class="form-control" id="nome" name="nome" required>
                 </div>
 
                 <div class="mb-3">
                     <label for="fabricante" class="form-label">Fabricante</label>
                     <select class="form-select" aria-label="Default select example" id="fabricante" name="fabricante">
                         <option value="" selected disabled>Selecione uma marca</option>
-                        <option value="1">Ferrari</option>
-                        <option value="2">Audi</option>
-                        <option value="3">Ford</option>
-                        <option value="4">Bugatti</option>
+                        <?php foreach ($fabricantes as $key => $value) : ?>
+                            <option value="<?= $key ?>"><?= $value ?></option>
+                        <?php endforeach; ?>
                     </select>
                 </div>
 
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Preço</label>
-                    <input type="number" class="form-control" id="exampleFormControlInput1" name="preco" min="100" max="10000" step=".01">
+                    <input type="number" class="form-control" id="exampleFormControlInput1" name="preco" min="100" max="10000" step=".01" required>
                 </div>
                 <div class="mb-3">
                     <label for="sim" class="form-label">Disponibilidade: </label>
@@ -104,6 +137,7 @@
             </form>
         </div>
         <?php } ?>
+
 
     </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
